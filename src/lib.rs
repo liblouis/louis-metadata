d4b7bed1fdcb9593441be647963291a9b677af65
Christian Egli <christian.egli@sbs.ch>
2020-05-13 14:50:33 +0200

    Implement indexing a directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,52 @@
-use std::fs;
-use std::error::Error;
 use regex::Regex;
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
 
 use std::collections::HashMap;
 
-pub fn run(filename: String) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(filename)?;
-
-    for (k, v) in search(&contents) {
-        println!("{}: {}", k, v);
+pub fn run(path: String) -> Result<(), Box<dyn Error>> {
+    for (path, metadata) in index_path(&path) {
+        println!("== {}", path.to_str().unwrap());
+        for (k, v) in metadata {
+            println!("{}: {}", k, v);
+        }
     }
 
     Ok(())
 }
 
-pub fn search(contents: &str) -> HashMap<&str, &str> {
+pub fn analyze_table(contents: &str) -> HashMap<String, String> {
     let re = Regex::new(r"^#(\+|-)(?P<key>[-[:lower:]]+):\s*(?P<value>.+)$").unwrap();
 
     let mut metadata = HashMap::new();
 
     for line in contents.lines() {
-	if let Some(caps) = re.captures(line) {
-	    let k = caps.name("key").unwrap().as_str();
-	    let v = caps.name("value").unwrap().as_str();
-	    metadata.insert(k, v);
-	}
+        if let Some(caps) = re.captures(line) {
+            let k = caps.name("key").unwrap().as_str().to_string();
+            let v = caps.name("value").unwrap().as_str().to_string();
+            metadata.insert(k, v);
+        }
     }
     metadata
+}
+
+pub fn index_path(path: &str) -> HashMap<PathBuf, HashMap<String, String>> {
+    let mut metadata_index = HashMap::new();
+
+    for entry in fs::read_dir(path).unwrap() {
+        let path = entry.unwrap().path();
+        if path.is_file() {
+            // ignore files that aren't utf-8
+            if let Ok(content) = fs::read_to_string(&path) {
+                let metadata = analyze_table(&content);
+                if !metadata.is_empty() {
+                    metadata_index.insert(path, metadata);
+                }
+            }
+        }
+    }
+    metadata_index
 }
 
 #[cfg(test)]
@@ -43,13 +63,13 @@ mod tests {
 #+foo_bar: no
 #-contracted: full";
 
-	let map = hashmap!{
-	    "foo" => "bar",
-	    "hehe" => "hoho",
-	    "foo-bar" => "yes",
-	    "contracted" => "full",
-	};
-        assert_eq!(map, search(contents));
+        let map = hashmap! {
+            String::from("foo") => String::from("bar"),
+            String::from("hehe") => String::from("hoho"),
+            String::from("foo-bar") => String::from("yes"),
+            String::from("contracted") => String::from("full"),
+        };
+        assert_eq!(map, analyze_table(contents));
     }
 
     #[test]
@@ -58,7 +78,7 @@ mod tests {
 #+: no
 #+foo-bar:";
 
-	let map = hashmap!{};
-        assert_eq!(map, search(contents));
+        let map = hashmap! {};
+        assert_eq!(map, analyze_table(contents));
     }
 }
